e6a635e1d13eab274df06d2fd7a12e124290358c
DESKTOP-J4DM5UA\Otmars <ootmar1998@gmail.com>
2024-10-17 00:58:58 -0400

    animaciones

--- src/app/ladingpage/ladingpage.component.ts
+++ src/app/ladingpage/ladingpage.component.ts
@@ -1,8 +1,9 @@
 import { NoticiasComponent } from './../noticias/noticias.component';
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { LoginComponent } from '../login/login.component';
 import { ButtonModule } from 'primeng/button';
+import ScrollReveal from 'scrollreveal';
 
 @Component({
   selector: 'app-ladingpage',
@@ -11,6 +12,36 @@ import { ButtonModule } from 'primeng/button';
   templateUrl: './ladingpage.component.html',
   styleUrl: './ladingpage.component.css'
 })
-export class LadingpageComponent {
-
+export class LadingpageComponent  implements AfterViewInit{
+  ngAfterViewInit(): void {
+    ScrollReveal().reveal('.background-image, .header', {
+      duration: 1000,
+      delay: 10,
+      origin: 'bottom',
+      distance: '100px',
+      scale: 1,
+      easing: 'ease-in-out',
+      mobile: true,
+      reset: false,
+    });
+    ScrollReveal().reveal('.content_left', {
+      duration: 1000,
+      delay: 100,
+      origin: 'left',
+      distance: '100px',
+    });
+    ScrollReveal().reveal('.img-logo', {
+      duration: 1000,
+      delay: 100,
+      origin: 'right',
+      distance: '100px',
+    });
+    ScrollReveal().reveal('.noticias', {
+      duration: 1000,
+      delay: 1000,
+      origin: 'top',
+      distance: '100px',
+      reset: true,
+    });
+  }
 }
